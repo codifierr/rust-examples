d14d1b6fb617d87a2816825dd41116ae76d06784
Satyendra Singh <satyendra.singh@hpe.com>
2022-10-31 18:11:40 +0530

    #fixing build

--- build.sh
+++ build.sh
@@ -1,12 +1,39 @@
 cd hello-rust 
-cargo clean
+cargo build --release
+status=$?
+if [ $status -eq 0 ]
+then
+  echo "Compilation succeeded."
+else
+  cargo clean
+  echo "Error: Compilation Failed."
+  exit $status
+fi
 cd ..
 
 cd http-actix-ping
-cargo clean
+cargo build --release
+status=$?
+if [ $status -eq 0 ]
+then
+  echo "Compilation succeeded."
+else
+  cargo clean
+  echo "Error: Compilation Failed."
+  exit $status
+fi
 cd ..
 
 cd http-rocket-ping
-cargo clean
+cargo build --release
+status=$?
+if [ $status -eq 0 ]
+then
+  echo "Compilation succeeded."
+else
+  cargo clean
+  echo "Error: Compilation Failed."
+  exit $status
+fi
 cd ..
 
